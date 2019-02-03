fb47c73b8118e7bad8b54379f29584fd4a83b506
prosis369 <pniharika369@gmail.com>
2019-02-03 22:26:16 +0530

    Spoj

---
+++ number_steps.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	while(n)
+	{
+		int x,y;
+		cin>>x;
+		cin>>y;
+		if(x==y)
+		{
+			if(x%2 == 0)
+			{
+				cout<<x+y<<"\n";
+			}
+			else
+			{
+				cout<<x+y-1<<"\n";
+			}
+		}
+		else if(x-y == 2)
+		{
+			if((x%2 == 0) && ((y%2 == 0) || (y == 0)))
+			{
+				cout<<x+y<<"\n";
+			}
+			else
+			{
+				cout<<x+y-1<<"\n";
+			}
+		}
+		else
+		{
+			cout<<"No Number\n";
+		}
+		n--;
+	}
+}
