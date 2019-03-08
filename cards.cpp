7971cef9ac1f2b49f80e8118e052096da0d039f0
prosis369 <pniharika369@gmail.com>
2019-03-08 19:10:08 +0530

    Spoj

---
+++ cards.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long int n;
+		cin>>n;
+		long long int ans1 = n*(n+1);
+		long long int ans2 = n*(n-1)/2;
+		long long int ans = (ans1+ans2) % 1000007;
+		cout<<ans<<endl;
+	}
+}
