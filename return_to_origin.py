9df9399cd2d5a1be753482d22aa5d9edc29af3b8
Niharika <30532525+prosis369@users.noreply.github.com>
2020-04-08 10:23:56 +0530

    leetcode

---
+++ return_to_origin.py
@@ -0,0 +1,31 @@
+'''
+
+657. Robot Return to Origin
+
+There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+
+Example 1:
+Input: "UD"
+Output: true 
+Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+
+'''
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        start = [0,0]
+        for i in moves:
+            if i == 'L':
+                start[0] = start[0]-1
+            elif i =='R':
+                start[0] = start[0]+1
+            elif i == 'U':
+                start[1] = start[1]+1
+            else:
+                start[1] = start[1]-1
+        if start == [0,0]:
+            return True
+        else:
+            return False
