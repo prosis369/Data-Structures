bdb066012f1806969e268a0ab1cf69e8a864e35f
Niharika <30532525+prosis369@users.noreply.github.com>
2020-07-05 00:33:41 +0530

    GeeksForGeeks

---
+++ binary_strings.py
@@ -0,0 +1,29 @@
+# Generate all binary strings without consecutive 1’s
+
+def generate(result, x, n):
+
+	if(n == x):
+		print(*result, sep="", end=" ")
+		return
+
+	if(result[n-1] == '0'):
+		result[n] = '0'
+		generate(result,x,n+1)
+		result[n] = '1'
+		generate(result,x,n+1)
+
+	if(result[n-1] == '1'):
+		result[n] = '0'
+		generate(result,x,n+1)
+
+
+x = int(input("Enter the size the string: "))
+result = [0]*x
+
+result[0] = '0'
+generate(result, x, 1)
+
+result[0] = '1'
+generate(result, x, 1)
+
+print("\n")
