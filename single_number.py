c21633ca79bb8c65049afe4268762aa09b027f6b
Niharika <30532525+prosis369@users.noreply.github.com>
2020-07-29 13:12:41 +0530

    LeetCode

--- single_number.py
+++ single_number.py
@@ -5,7 +5,7 @@ Given a non-empty array of integers, every element appears twice except for one.
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
-Example 1:
+Example:
 
 Input: [2,2,1]
 Output: 1
